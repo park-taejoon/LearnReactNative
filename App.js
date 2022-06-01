bed0bf7b2751dc1e65e039f164b215c5c5538dc3
pyh1987 <pyh1987@naver.com>
2022-06-02 01:18:15 +0900

    hook 관리

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import Box from './components/Box';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter.js';
 {
   /*import Greeting from './components/Greeting'; */
 }
 
 const App = () => {
-  //const [state, setState]
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
-  };
+  const [count, setCount] = useState(0);
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
       {/* 주석을 작성해보자*/}
       {/* <Greeting
         name={name} //이름을 설정하기
       /> */}
-      <Button title="토클" onPress={onPress} />
-      {visible && <Box rounded={true} size="large" color="blue" />}
+      {/* <Button title="토글" onPress={onPress} />
+      {visible && <Box rounded={true} size="large" color="blue" />} */}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {flex: 1},
+});
 
 export default App;
